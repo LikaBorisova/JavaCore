d336f3d94ab211b1a2f9f900f06613b9c6862141
LikaBorisova <likaqait@gmail.com>
2022-06-16 22:39:19 +0700

    DZ 5

---
+++ src/Lesson5/MyFileData.java
@@ -0,0 +1,14 @@
+package Lesson5;
+
+public class MyFileData {
+    private String[] header;
+    private int[][] data;
+
+    public void setHeader(String[] header) {
+        this.header = header;
+    }
+
+    public void setData(int[][] data) {
+        this.data = data;
+    }
+}
